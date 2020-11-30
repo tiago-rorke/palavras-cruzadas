5947b23cb405c2be6194da7d34d7b06a36eb35c3
tiago-rorke <hello@tiago.nz>
2020-11-30 21:49:59 +0000

    add plotter client

---
+++ plotter-client/index.js
@@ -0,0 +1,60 @@
+
+const express = require('express');
+const socketIO = require('socket.io');
+
+const SerialPort = require('serialport')
+const { GrblStream } = require('grbl-stream')
+
+const port = new SerialPort('/dev/ttyUSB0', { baudRate: 115200})
+const grbl = new GrblStream()
+
+const app = express();
+const server = app
+	.listen(3000, () => console.log(`Listening on 3000`));
+
+const io = socketIO(server);
+
+app.use(express.static('/'));
+app.get('/', function (req, res){
+	 res.sendFile(__dirname + '/index.html');
+});
+
+grbl.pipe(port).pipe(grbl)
+  .on('command', cmd => console.log('>', cmd))
+  .on('message', msg => console.log('<', msg))
+
+io.on('connection', (socket) => {
+	console.log('Client connected');
+
+	socket.on('get_status', async () => {
+		console.log('get_status');
+		let status = await grbl.status();
+		io.emit('status', status);
+	});
+
+	socket.on('unlock', async () => {
+		console.log('unlock');
+		await grbl.killAlarmLock();
+	});
+
+	socket.on('home', async () => {
+		console.log('home');
+		await grbl.runHomingCycle();
+	});
+
+});
+
+
+// grbl.command('?');
+// grbl.command('$$');
+
+/*
+console.log('status', await grbl.status())
+console.log('help', await grbl.help())
+console.log('settings', await grbl.settings())
+
+await grbl.runHomingCycle()
+await grbl.killAlarmLock()
+await grbl.metricCoordinates()
+await grbl.incrementalPositioning()
+await grbl.position({ x: -100, y: -100 })*/
