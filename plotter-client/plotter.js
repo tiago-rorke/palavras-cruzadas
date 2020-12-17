f1500817587bec220ad650d74f861629bd7e1db6
tiago-rorke <hello@tiago.nz>
2020-12-17 15:06:50 +0000

    add plotter.js

---
+++ plotter-client/plotter.js
@@ -0,0 +1,58 @@
+"use strict";
+
+const SerialPort = require('serialport')
+const { GrblStream } = require('grbl-stream')
+
+
+// grbl.command('?');
+// grbl.command('$$');
+
+/*
+console.log('status', await grbl.status())
+console.log('help', await grbl.help())
+console.log('settings', await grbl.settings())
+
+await grbl.runHomingCycle()
+await grbl.killAlarmLock()
+await grbl.metricCoordinates()
+await grbl.incrementalPositioning()
+await grbl.position({ x: -100, y: -100 })
+*/
+
+
+// ========================== PLOTTER CLASS =============================== //
+
+const internal = {};
+
+internal.Plotter = class {
+
+   constructor(port, baud) {
+   	this.port = new SerialPort(port, { baudRate: baud})
+		this.grbl = new GrblStream()
+
+		this.grbl.pipe(this.port).pipe(this.grbl)
+		   .on('command', cmd => console.log('>', cmd))
+		   .on('message', msg => console.log('<', msg))
+   }
+
+   async home() {
+   	await this.grbl.runHomingCycle();
+   	return new Promise((resolve, reject) => {
+         resolve(true);
+      });
+   }
+	async unlock() {
+   	await this.grbl.killAlarmLock();
+   	return new Promise((resolve, reject) => {
+         resolve(true);
+      });
+   }
+}
+/*		await this.grbl.killAlarmLock()
+		await this.grbl.metricCoordinates()
+		await this.grbl.incrementalPositioning()
+		await this.grbl.position({ x: -100, y: -100 })*/
+
+// ====================================================== //
+
+module.exports = internal.Plotter;
